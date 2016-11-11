75b5f6acc441efe66c22b548ea21c5a16210af33
John Martinsson <john.martinsson@gmail.com>
2016-11-11 11:46:35 +0100

    Update cuberun model with BN, and input shape.

--- bird/models/cuberun.py
+++ bird/models/cuberun.py
@@ -1,44 +1,93 @@
-import json
-from keras.models import Sequential
+from keras.layers import Input
 from keras.layers import Dense, Activation, Dropout, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
 
-model = Sequential()
+def CubeRun(nb_classes):
+    """ Instantiate a CubeRun architecture
 
-# conv (64 5x5 kernels, stride size 2x1)
-# TODO : 1 channel?
-model.add(Convolution2D(64, 5, 5, input_shape=(3, 128, 256), activation="relu", subsample=(2, 1)))
-# max pooling (2x2 kernels, stride size 2x2)
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # Arguments
+        nb_classes: the number of classification classes
 
-# conv (64 5x5 kernels, stride size 1x1)
-model.add(Convolution2D(64, 5, 5, activation="relu", subsample=(1, 1)))
-# max pooling (2x2 kernels, stride size 2x2)
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # Returns
+        A Keras model instance
+    """
 
-# conv (128 5x5 kernels, stride size 1x1)
-model.add(Convolution2D(128, 5, 5, activation="relu", subsample=(1, 1)))
-# max pooling (2x2 kernels, stride size 2x2)
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # adapt input shape to the used backend
+    if K.image_dim_ordering() == 'th':
+        input_shape=(1, 257, 624)
+    else:
+        input_shape=(257, 624, 1)
 
-# conv (256 5x5 kernels, stride size 1x1)
-model.add(Convolution2D(256, 5, 5, activation="relu", subsample=(1, 1)))
-# max pooling (2x2 kernels, stride size 2x2)
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    img_input = Input(shape=input_shape)
 
-# conv (256 3x3 kernels, stride size 1x1)
-model.add(Convolution2D(256, 3, 3, activation="relu", subsample=(1, 1)))
-# max pooling (2x2 kernels, stride size 2x2)
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # adapt back normalization axis to the used backend
+    if K.image_dim_ordering() == 'th':
+        bn_axis = 1
+    else:
+        bn_axis = 3
 
-# flatten 3D feature maps to 1D feature vectors
-model.add(Flatten())
+    x = ZeroPadding2D((2, 2))(img_input)
+    #x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    #x = Dropout(0.2)(x)
 
-# dense (1024 units)
-model.add(Dense(1024))
-# soft max (19 units)
-model.add(Dense(19, activation='softmax'))
+    # conv (64 5x5 kernels, stride size 2x1)
+    x = Convolution2D(64, 5, 5, subsample=(2, 1))(x)
+    x = Activation('relu')(x)
+    # max pooling (2x2 kernels, stride size 2x2)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
 
-with open('cuberun.json', 'w') as outfile:
-    json.dump(mode.to_json(), outfile)
+    # batch normalization
+    x = BatchNormalization(axis=bn_axis)(x)
+    # conv (64 5x5 kernels, stride size 1x1)
+    x = Convolution2D(64, 5, 5, subsample=(1, 1))(x)
+    x = Activation('relu')(x)
+    # max pooling (2x2 kernels, stride size 2x2)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    # batch normalization
+    x = BatchNormalization(axis=bn_axis)(x)
+    # conv (128 5x5 kernels, stride size 1x1)
+    x = Convolution2D(128, 5, 5, subsample=(1, 1))(x)
+    x = Activation('relu')(x)
+    # max pooling (2x2 kernels, stride size 2x2)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    # batch normalization
+    x = BatchNormalization(axis=bn_axis)(x)
+    # conv (256 5x5 kernels, stride size 1x1)
+    x = Convolution2D(256, 5, 5, activation="relu", subsample=(1, 1))(x)
+    x = Activation('relu')(x)
+    # max pooling (2x2 kernels, stride size 2x2)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    # batch normalization
+    x = BatchNormalization(axis=bn_axis)(x)
+    # conv (256 3x3 kernels, stride size 1x1)
+    x = Convolution2D(256, 3, 3, subsample=(1, 1))(x)
+    x = Activation('relu')(x)
+    # max pooling (2x2 kernels, stride size 2x2)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # batch normalization
+    #x = BatchNormalization(axis=bn_axis)(x)
+
+    # flatten 3D feature maps to 1D feature vectors
+    x = Flatten()(x)
+
+    # dense layer
+    x = Dense(1024)(x)
+    x = Activation('relu')(x)
+    # dense layer dropout
+    x = Dropout(0.4)(x)
+
+    # soft max layer
+    x = Dense(nb_classes)(x)
+    x = Activation('softmax')(x)
+    # soft max layer dropout
+    #x = Dropout(0.4)(x)
+
+    model = Model(img_input, x)
+
+    return model
