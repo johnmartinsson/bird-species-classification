593929e02360d080ec693aee013c2cae1beb7cf1
John Martinsson <john.martinsson@gmail.com>
2016-11-17 23:46:34 +0100

    Add and document utillity functions

--- bird/utils.py
+++ bird/utils.py
@@ -3,14 +3,33 @@ import os
 import sys
 import subprocess
 import wave
-import wave
 from scipy import signal
 from scipy import fft
 from matplotlib import pyplot as plt
+from functools import reduce
+
+import preprocessing as pp
 
 MLSP_DATA_PATH="/home/darksoox/gits/bird-species-classification/mlsp_contest_dataset/"
 
+def test(filename):
+    fs, x = read_wave_file(filename)
+    (t, f, Sxx) = wave_to_spectrogram(x, fs)
+    #noise = pp.extract_noise_part(Sxx)
+    plot_matrix(Sxx, "Spectrogram")
+    signal = pp.extract_signal_part(Sxx)
+
+    #plot_matrix(noise, "Noise")
+    #plot_matrix(signal, "Signal")
+
+def plot_spectrogram_from_wave(filename):
+    fs, x = read_wave_file(filename)
+    (t, f, Sxx) = wave_to_spectrogram(x, fs)
+    plot_matrix(Sxx, "Spectrogram")
+
 def play_wave_file(filename):
+    """ Play a wave file
+    """
     if (not os.path.isfile(filename)):
         raise ValueError("File does not exist")
     else:
@@ -20,7 +39,12 @@ def play_wave_file(filename):
             print("Platform not supported")
 
 def read_wave_file(filename):
-
+    """ Read a wave file from disk
+    # Arguments
+        filename : the name of the wave file
+    # Returns
+        (fs, x)  : (sampling frequency, signal)
+    """
     if (not os.path.isfile(filename)):
         raise ValueError("File does not exist")
 
@@ -40,14 +64,27 @@ def read_wave_file(filename):
 
 def wave_to_spectrogram(wave=np.array([]), fs=None, nperseg=512, noverlap=384):
     """Given a wave form returns the spectrogram of the wave form.
-
-    Keyword arguments:
-    wave -- the wave form (default np.array([]))
-    fs   -- the rate at which the wave form has been sampled
+    # Arguments
+        wave : the wave form (default np.array([]))
+        fs   : the rate at which the wave form has been sampled
+    # Returns
+        spectrogram : the computed spectrogram (numpy array)
     """
     window = signal.get_window('hanning', nperseg)
     return signal.spectrogram(wave, fs, window, nperseg, noverlap,
                               mode='magnitude')
+
+def additively_combine_narrays(narrays):
+    """ Additively combine and rescale two narrays
+    # Arguments
+        narrays : the narrays to combine
+    # Returns
+        out     : the combined and rescaled narray
+    """
+    nb_narrays = narrays.shape[0]
+    norm_factor = 1.0/nb_narrays
+    narray = reduce(lambda a1, a2: norm_factor*a1 + norm_factor*a2, narrays)
+    return narray
 
 def wave_to_spectrogram2(S):
     Spectrogram = []
@@ -62,13 +99,14 @@ def wave_to_spectrogram2(S):
 
     return np.array(Spectrogram)
 
-def plot_matrix(Sxx):
+def plot_matrix(Sxx, title):
     cmap = grayify_cmap('cubehelix_r')
     #cmap = plt.cm.get_cmap('gist_rainbow')
-    plt.figure()
+    fig = plt.figure()
+    fig.suptitle(title, fontsize=12)
     plt.pcolormesh(Sxx, cmap=cmap)
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [s]')
+    plt.ylabel('Frequency Bins')
+    plt.xlabel('Samples')
     plt.show()
 
 def plot_vector(x):
