bfdf659262309afbc371488408e13cb1efc04015
John Martinsson <john.martinsson@gmail.com>
2016-11-18 13:59:14 +0100

    Finish CubeRun model

--- bird/models/cuberun.py
+++ bird/models/cuberun.py
@@ -5,7 +5,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras import backend as K
 
-def CubeRun(nb_classes=19, input_shape=(257, 624, 1)):
+def CubeRun(nb_classes, input_shape):
     """ Instantiate a CubeRun architecture
 
     # Arguments
@@ -16,22 +16,23 @@ def CubeRun(nb_classes=19, input_shape=(257, 624, 1)):
     """
 
     # adapt input shape to the used backend
-    #if K.image_dim_ordering() == 'th':
-        #input_shape=(1, image_rows, image_cols)
-    #else:
-        #input_shape=(image_rows, image_cols, 1)
+    (image_rows, image_cols) = input_shape
+    if K.image_dim_ordering() == 'th':
+        input_shape=(1, image_rows, image_cols)
+    else:
+        input_shape=(image_rows, image_cols, 1)
 
     img_input = Input(shape=input_shape)
 
-    # adapt back normalization axis to the used backend
+    # adapt batch normalization axis to the used backend
     if K.image_dim_ordering() == 'th':
         bn_axis = 1
     else:
         bn_axis = 3
 
     x = ZeroPadding2D((2, 2))(img_input)
-    #x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
-    #x = Dropout(0.2)(x)
+    x = Dropout(0.2)(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
 
     # conv (64 5x5 kernels, stride size 2x1)
     x = Convolution2D(64, 5, 5, subsample=(2, 1))(x)
@@ -70,23 +71,20 @@ def CubeRun(nb_classes=19, input_shape=(257, 624, 1)):
     x = Activation('relu')(x)
     # max pooling (2x2 kernels, stride size 2x2)
     x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
-    # batch normalization
-    #x = BatchNormalization(axis=bn_axis)(x)
 
     # flatten 3D feature maps to 1D feature vectors
     x = Flatten()(x)
 
     # dense layer
+    x = Dropout(0.4)(x)
     x = Dense(1024)(x)
     x = Activation('relu')(x)
     # dense layer dropout
-    x = Dropout(0.4)(x)
 
     # soft max layer
+    x = Dropout(0.4)(x)
     x = Dense(nb_classes)(x)
-    x = Activation('sigmoid')(x)
-    # soft max layer dropout
-    #x = Dropout(0.4)(x)
+    x = Activation('softmax')(x)
 
     model = Model(img_input, x)
 
