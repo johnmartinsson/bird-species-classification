0ad33140ecc1aca1aa59e2fa25896e9e37b7ff53
John Martinsson <john.martinsson@gmail.com>
2016-11-23 19:15:49 +0100

    Add noise/signal wave preprocessing

--- bird/utils.py
+++ bird/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import csv
 import glob
 import sys
 import subprocess
@@ -10,31 +11,44 @@ from scipy.io import wavfile
 from matplotlib import pyplot as plt
 from functools import reduce
 
-import preprocessing as pp
+import bird.preprocessing as pp
+import bird.loader as loader
 
-MLSP_DATA_PATH="/home/darksoox/gits/bird-species-classification/mlsp_contest_dataset/"
+def get_basename_without_ext(filepath):
+    basename = os.path.splitext(os.path.basename(filepath))[0]
+    return basename
 
 def preprocess_data_set(data_path, output_directory):
     wave_files = glob.glob(os.path.join(data_path, "*.wav"))
+    file2labels_path = os.path.join(data_path, "file2labels.csv")
 
-    for f in wave_files:
-        print ("Preprocessing file: ", f)
-        preprocess_sound_file(f, output_directory)
+    print(file2labels_path)
+    file2labels = loader.read_file2labels(file2labels_path);
+    file2labels_path_new = os.path.join(output_directory, "file2labels.csv")
 
+    with open(file2labels_path_new, 'w') as file2labels_csv:
+        file2labelswriter = csv.writer(file2labels_csv)
+        for f in wave_files:
+            print ("Preprocessing file: ", f)
+            basename = get_basename_without_ext(f)
+            labels = file2labels[basename]
+            preprocess_sound_file(f, output_directory, labels,
+                                  file2labelswriter)
 
-def preprocess_sound_file(filename, output_directory):
-    basename = os.path.splitext(os.path.basename(filename))[0]
-    fs, x = read_wave_file(filename)
-    (t, f, Sxx) = wave_to_spectrogram(x, fs)
+def preprocess_wave(wave, fs):
+    (t, f, Sxx) = wave_to_spectrogram(wave, fs)
 
     n_mask = pp.compute_noise_mask(Sxx)
     s_mask = pp.compute_signal_mask(Sxx)
 
-    n_mask_scaled = pp.reshape_binary_mask(n_mask, x.shape[0])
-    s_mask_scaled = pp.reshape_binary_mask(s_mask, x.shape[0])
+    n_mask_scaled = pp.reshape_binary_mask(n_mask, wave.shape[0])
+    s_mask_scaled = pp.reshape_binary_mask(s_mask, wave.shape[0])
 
-    signal_wave = pp.extract_masked_part_from_wave(s_mask_scaled, x)
-    noise_wave = pp.extract_masked_part_from_wave(n_mask_scaled, x)
+    #print ("Signal shape: ", x.shape)
+    #print ("Noise mask shape: ", n_mask_scaled.shape)
+
+    signal_wave = pp.extract_masked_part_from_wave(s_mask_scaled, wave)
+    noise_wave = pp.extract_masked_part_from_wave(n_mask_scaled, wave)
 
     chunk_size = 512 * 128
     signal_wave_padded = zero_pad_wave(signal_wave, chunk_size)
@@ -43,12 +57,20 @@ def preprocess_sound_file(filename, output_directory):
     signal_chunks = split_into_chunks(signal_wave_padded, chunk_size)
     noise_chunks = split_into_chunks(noise_wave_padded, chunk_size)
 
+    return signal_chunks, noise_chunks
+
+def preprocess_sound_file(filename, output_directory, labels, file2labelswriter):
+    basename = os.path.splitext(os.path.basename(filename))[0]
+    fs, x = read_wave_file(filename)
+    signal_chunks, noise_chunks = preprocess_wave(x, fs)
+
     i_chunk = 0
     for s in signal_chunks:
         filename_chunk = os.path.join(output_directory, basename +
                                       "_signal_chunk_" + str(i_chunk) + ".wav")
         print (filename_chunk)
         write_wave_to_file(filename_chunk, fs, s)
+        file2labelswriter.writerow([get_basename_without_ext(filename_chunk)] + labels)
         i_chunk += 1
 
     i_chunk = 0
@@ -58,10 +80,6 @@ def preprocess_sound_file(filename, output_directory):
         print (filename_chunk)
         write_wave_to_file(filename_chunk, fs, s)
         i_chunk += 1
-
-    (t, f, Sxx_signal) = wave_to_spectrogram(signal_wave, fs)
-    (t, f, Sxx_noise) = wave_to_spectrogram(noise_wave, fs)
-
 
 def split_into_chunks(array, chunk_size):
     nb_chunks = array.shape[0]/chunk_size
