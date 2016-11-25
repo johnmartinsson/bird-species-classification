e69f709c39da9413a7053f787e417ae3f47a985c
John Martinsson <john.martinsson@gmail.com>
2016-11-25 17:29:34 +0100

    Add a mini_batch_generator scheme
      - yields mini_batches of augmented training data samples

--- bird/loader.py
+++ bird/loader.py
@@ -1,14 +1,94 @@
 # Given path to .wav folder, and path to .csv file name -> class bag
 # Return training data X_train, Y_train
 
+import gc
 import csv
 import os
 import warnings
 import random
 import glob
 import numpy as np
+np.random.seed(42)
 
 from bird import utils
+from bird import data_augmentation as da
+
+def mini_batch_generator(nb_augmentation_samples, nb_mini_baches, batch_size,
+                         data_filepath, file2labels_filepath, nb_classes,
+                         samplerate):
+    i_batch_counter = 0
+    augmentation_set = create_augmentation_set(data_filepath,
+                                               file2labels_filepath,
+                                               nb_augmentation_samples)
+    while i_batch_counter < nb_mini_baches:
+        mini_batch = np.random.choice(augmentation_set, batch_size)
+        signals_and_labels = [da.apply_augmentation(d) for d in mini_batch]
+        # extract labels
+        Y_train_aux = [l for (s, l) in signals_and_labels]
+        # convert to waves
+        X_train_aux = [s for (s, l) in signals_and_labels]
+        # convert to spectrograms
+        X_train = [utils.wave_to_spectrogram_aux(s, samplerate) for s in X_train_aux]
+        # convert to binary labels
+        Y_train = [id_labels2binary_labels(l, nb_classes) for l in Y_train_aux]
+        # force the garbage collector to clear unreferenced memory
+        gc.collect()
+        Y_train = np.array(Y_train)
+        X_train = np.array(X_train)
+        X_train = X_train.reshape(X_train.shape[0], X_train.shape[1],
+                                            X_train.shape[2], 1)
+        yield  X_train, Y_train
+        i_batch_counter += 1
+
+def load_noise_segment_filenames(data_filepath):
+    noise_segment_filenames = [utils.get_basename_without_ext(f) for f in
+                           glob.glob(os.path.join(data_filepath, "noise",
+                                                  "*.wav"))]
+    return noise_segment_filenames
+
+def load_signal_segment_filenames(data_filepath):
+    signal_segment_filenames = [utils.get_basename_without_ext(f) for f in
+                            glob.glob(os.path.join(data_filepath, "*.wav"))]
+    return signal_segment_filenames
+
+
+def create_augmentation_set(data_filepath, file2labels_filepath, nb_augmentation_dicts):
+    signal_segment_filenames = load_signal_segment_filenames(data_filepath)
+    noise_segment_filenames = load_noise_segment_filenames(data_filepath)
+    file2labels = read_file2labels(file2labels_filepath)
+
+    augmentation_set = []
+    for i in range(nb_augmentation_dicts):
+        signal_segment_filename = np.random.choice(signal_segment_filenames, 1)[0]
+        augmentation_dict = da.create_augmentation_dict(signal_segment_filename,
+                                                        noise_segment_filenames, file2labels)
+        augmentation_dict = reconstruct_relative_paths(augmentation_dict, data_filepath)
+        augmentation_set.append(augmentation_dict)
+
+    return augmentation_set
+
+def reconstruct_relative_paths(augmentation_dict, data_filepath):
+    signal_filename = augmentation_dict['signal_filename']
+    augmentation_signal_filename = augmentation_dict['augmentation_signal_filename']
+    augmentation_noise_filenames = augmentation_dict['augmentation_noise_filenames']
+    labels = augmentation_dict['labels']
+
+    signal_filepath = os.path.join(data_filepath, signal_filename+".wav")
+    augmentation_signal_filepath = os.path.join(data_filepath,
+                                                augmentation_signal_filename
+                                                +".wav")
+    augmentation_noise_filepaths = [os.path.join(data_filepath, "noise",
+                                                 noise_filename+".wav") for
+                                                 noise_filename in
+                                                 augmentation_noise_filenames]
+    augmentation_segment = {
+        'signal_filepath':signal_filepath,
+        'augmentation_signal_filepath':augmentation_signal_filepath,
+        'augmentation_noise_filepaths':augmentation_noise_filepaths,
+        'labels':labels
+    }
+
+    return augmentation_segment
 
 def read_file2labels(file2labels_filepath):
     labels = {}
