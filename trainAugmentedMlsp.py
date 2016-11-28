4d116208aff77b2ff46e364a30d75252734dfae4
John Martinsson <john.martinsson@gmail.com>
2016-11-28 18:18:25 +0100

    Change to F-score instead of accuracy

--- trainAugmentedMlsp.py
+++ trainAugmentedMlsp.py
@@ -36,7 +36,7 @@ model = CubeRun(nb_classes=nb_classes, input_shape=input_shape)
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='binary_crossentropy',
               optimizer='adadelta',
-              metrics=['accuracy'])
+              metrics=['fbeta_score'])
 
 print (strftime("%a, %d %b %Y %H:%M:%S +0000", localtime()))
 # load the data
