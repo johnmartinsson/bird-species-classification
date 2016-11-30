939c4000370ac1ef91a541b9b57fd48d168b7d3f
John Martinsson <john.martinsson@gmail.com>
2016-11-30 15:19:41 +0100

    Add log magnitude spectrogram, and time/pitch shift in spectral domain

--- bird/utils.py
+++ bird/utils.py
@@ -21,30 +21,6 @@ def get_basename_without_ext(filepath):
     basename = os.path.basename(filepath).split(os.extsep)[0]
     return basename
 
-def test(filename):
-    fs, x = read_gzip_wave_file(filename)
-    (t, f, Sxx) = wave_to_spectrogram(x, fs)
-    #noise = pp.extract_noise_part(Sxx)
-    #plot_matrix(Sxx, "Spectrogram")
-
-    n_mask = pp.compute_noise_mask(Sxx)
-    s_mask = pp.compute_signal_mask(Sxx)
-
-    n_mask_scaled = pp.reshape_binary_mask(n_mask, x.shape[0])
-    s_mask_scaled = pp.reshape_binary_mask(s_mask, x.shape[0])
-
-    signal_wave = pp.extract_masked_part_from_wave(s_mask_scaled, x)
-    noise_wave = pp.extract_masked_part_from_wave(n_mask_scaled, x)
-
-    signal_wave_padded = zero_pad_wave(signal_wave)
-    noise_wave_padded = zero_pad_wave(noise_wave)
-
-    (t, f, Sxx_signal) = wave_to_spectrogram(signal_wave, fs)
-    (t, f, Sxx_noise) = wave_to_spectrogram(noise_wave, fs)
-
-    #plot_matrix(Sxx_signal, "Signal Spectrogram")
-    #plot_matrix(Sxx_noise, "Noise Spectrogram")
-
 def plot_spectrogram_from_wave(filename):
     fs, x = read_gzip_wave_file(filename)
     (t, f, Sxx) = wave_to_spectrogram(x, fs)
@@ -122,6 +98,12 @@ def wave_to_spectrogram_aux(wave, fs):
     (f, t, Sxx) = wave_to_spectrogram(wave, fs)
     return Sxx
 
+def wave_to_log_spectrogram_aux(wave, fs):
+    """ Compute a log magnitude spectrogram from the given signal
+    """
+    Sxx = wave_to_spectrogram(wave, fs)
+    return np.log(Sxx)
+
 def compute_and_save_mask_as_image_from_file(filename):
     fs, x = read_gzip_wave_file(filename)
     t, f, Sxx = wave_to_spectrogram(x, fs)
@@ -129,48 +111,48 @@ def compute_and_save_mask_as_image_from_file(filename):
     mask = pp.compute_binary_mask(Sxx, 3.0, True, basename+".png")
 
 #def subplot_image(Sxx, n_subplot, title):
-#    cmap = grayify_cmap('cubehelix_r')
-#    plt.subplot(n_subplot)
-#    plt.title(title)
-#    plt.pcolormesh(Sxx, cmap=cmap)
-#
+    #cmap = grayify_cmap('cubehelix_r')
+    #plt.subplot(n_subplot)
+    #plt.title(title)
+    #plt.pcolormesh(Sxx, cmap=cmap)
+
 #def save_matrix_to_file(Sxx, title, filename):
-#    cmap = grayify_cmap('cubehelix_r')
-#    #cmap = plt.cm.get_cmap('gist_rainbow')
-#    fig = plt.figure()
-#    fig.suptitle(title, fontsize=12)
-#    plt.pcolormesh(Sxx, cmap=cmap)
-#    plt.ylabel('Frequency Bins')
-#    plt.xlabel('Samples')
-#    fig.savefig(filename)
-#
+    #cmap = grayify_cmap('cubehelix_r')
+    ##cmap = plt.cm.get_cmap('gist_rainbow')
+    #fig = plt.figure()
+    #fig.suptitle(title, fontsize=12)
+    #plt.pcolormesh(Sxx, cmap=cmap)
+    #plt.ylabel('Frequency Bins')
+    #plt.xlabel('Samples')
+    #fig.savefig(filename)
+
 #def plot_matrix(Sxx, title):
-#    cmap = grayify_cmap('cubehelix_r')
-#    #cmap = plt.cm.get_cmap('gist_rainbow')
-#    fig = plt.figure()
-#    fig.suptitle(title, fontsize=12)
-#    plt.pcolormesh(Sxx, cmap=cmap)
-#    plt.ylabel('Frequency Bins')
-#    plt.xlabel('Samples')
-#    plt.show()
-#
+    #cmap = grayify_cmap('cubehelix_r')
+    ##cmap = plt.cm.get_cmap('gist_rainbow')
+    #fig = plt.figure()
+    #fig.suptitle(title, fontsize=12)
+    #plt.pcolormesh(Sxx, cmap=cmap)
+    #plt.ylabel('Frequency Bins')
+    #plt.xlabel('Samples')
+    #plt.show()
+
 #def plot_vector(x):
-#    mesh = np.zeros((257, x.shape[0]))
-#    for i in range(x.shape[0]):
-#        mesh[256][i] = x[i] * 2500
-#        mesh[255][i] = x[i] * 2500
-#        mesh[254][i] = x[i] * 2500
-#    plot_matrix(mesh)
-#
+    #mesh = np.zeros((257, x.shape[0]))
+    #for i in range(x.shape[0]):
+        #mesh[256][i] = x[i] * 2500
+        #mesh[255][i] = x[i] * 2500
+        #mesh[254][i] = x[i] * 2500
+    #plot_matrix(mesh)
+
 #def grayify_cmap(cmap):
-#    """Return a grayscale version of the colormap"""
-#    cmap = plt.cm.get_cmap(cmap)
-#    colors = cmap(np.arange(cmap.N))
-#
-#    # convert RGBA to perceived greyscale luminance
-#    # cf. http://alienryderflex.com/hsp.html
-#    RGB_weight = [0.299, 0.587, 0.114]
-#    luminance = np.sqrt(np.dot(colors[:, :3] ** 2, RGB_weight))
-#    colors[:, :3] = luminance[:, np.newaxis]
-#
+    #"""Return a grayscale version of the colormap"""
+    #cmap = plt.cm.get_cmap(cmap)
+    #colors = cmap(np.arange(cmap.N))
+
+    ## convert RGBA to perceived greyscale luminance
+    ## cf. http://alienryderflex.com/hsp.html
+    #RGB_weight = [0.299, 0.587, 0.114]
+    #luminance = np.sqrt(np.dot(colors[:, :3] ** 2, RGB_weight))
+    #colors[:, :3] = luminance[:, np.newaxis]
+
 #    return cmap.from_list(cmap.name + "_grayscale", colors, cmap.N)
