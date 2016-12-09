3a5773cdeed34e6fda03ffe3a1f12c61f44eb144
John Martinsson <john.martinsson@gmail.com>
2016-12-09 01:28:57 +0100

    Update benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,11 @@
 from bird import preprocessing as pp
 from bird import utils
 
-filename = "datasets/mlsp2013/train/PC10_20090513_054500_0020.wav"
-(fs, x) = utils.read_wave_file(filename)
+filename = "datasets/mlsp2013/train/PC10_20090513_054500_0020.wav.gz"
+(fs, x) = utils.read_gzip_wave_file(filename)
 (t, f, Sxx) = utils.wave_to_spectrogram(x, fs)
 n_mask = pp.compute_signal_mask(Sxx)
 n_mask_scaled = pp.reshape_binary_mask(n_mask, x.shape[0])
-print(n_mask)
-print(n_mask_scaled)
 Nxx = pp.normalize(Sxx)
 
 def test():
@@ -36,20 +34,20 @@ def reshape_binary_mask():
 	pp.reshape_binary_mask(n_mask, 160000)
 
 def preprocess_wave():
-	utils.preprocess_wave(x, fs)
+	pp.preprocess_wave(x, fs)
 
-def read_wave_file():
-	utils.read_wave_file(filename)
+def read_gzip_wave_file():
+	utils.read_gzip_wave_file(filename)
 
-def mark_cells_times_larger_than_median():
-	pp.mark_cells_times_larger_than_median(Nxx, 3.0)
+def median_clipping():
+	pp.median_clipping(Nxx, 3.0)
 
 if __name__=='__main__':
 	import timeit
 	number = 10
 	print("compute_spectrogram():", timeit.timeit("compute_spectrogram()", setup="from __main__ import compute_spectrogram", number=number))
-	print("read_wave_file():", timeit.timeit("read_wave_file()", setup="from __main__ import read_wave_file", number=number))
-	print("mark_cells_times_larger_than_median():", timeit.timeit("mark_cells_times_larger_than_median()", setup="from __main__ import mark_cells_times_larger_than_median", number=number))
+	print("read_gzip_wave_file():", timeit.timeit("read_gzip_wave_file()", setup="from __main__ import read_gzip_wave_file", number=number))
+	print("median_clipping():", timeit.timeit("median_clipping()", setup="from __main__ import median_clipping", number=number))
 	print("compute_noise_mask():", timeit.timeit("compute_noise_mask()", setup="from __main__ import compute_noise_mask", number=number))
 	print("compute_signal_mask():", timeit.timeit("compute_signal_mask()", setup="from __main__ import compute_signal_mask", number=number))
 	print("reshape_binary_mask():", timeit.timeit("reshape_binary_mask()", setup="from __main__ import reshape_binary_mask", number=number))
