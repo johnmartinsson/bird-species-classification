9895a749f630ff298fe94fd8ed13c5118107cb84
johnmartinsson <john.martinsson@gmail.com>
2017-01-10 18:15:45 +0100

    Add separate noise directory argument

--- run_job.sh
+++ run_job.sh
@@ -4,5 +4,6 @@ source ./venv/bin/activate
 python3 -u ./train_model.py --weight_path=$1 --history_path=$2\
 				--train_path=$3 \
 				--valid_path=$4 \
-				--lock_file=$5\
-				--first_epoch=$6
+                                --noise_path=$5 \
+				--lock_file=$6 \
+				--first_epoch=$7
