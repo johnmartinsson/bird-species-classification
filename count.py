0fdc12dc530f9cd352fc04f17e194cd1ee74fde4
johnmartinsson <john.martinsson@gmail.com>
2017-01-16 16:41:21 +0100

    Add count script

--- count.py
+++ count.py
@@ -1,8 +1,9 @@
 import os
 import glob
 
-validation = "./datasets/birdClef2016Subset/valid"
-train = "./datasets/birdClef2016Subset/train"
+validation = "/disk/martinsson-spring17/birdClef2016Whole/valid"
+train = "/disk/martinsson-spring17/birdClef2016Whole/train"
+
 for (v, t) in zip(os.listdir(validation), os.listdir(train)):
     nb_valid = len(glob.glob(os.path.join(validation, v, "*.wav")))
     nb_train = len(glob.glob(os.path.join(train, t, "*.wav")))
