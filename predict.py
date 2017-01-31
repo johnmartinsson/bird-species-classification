c231cc4a54e792d1e0c5f8d4f9540670004505a5
John Martinsson <john.martinsson@gmail.com>
2017-01-31 12:30:01 +0100

    Add prediction module

--- predict.py
+++ predict.py
@@ -1,30 +1,29 @@
-from models.cuberun import CubeRun
+from bird import utils
+from bird.models.cuberun import CubeRun
+import bird.loader as loader
+import bird.signal_processing as sp
+import scipy
+
 import numpy as np
-import utils
 
-def get_model():
-    nb_classes = 19
-    input_shape = (257, 624, 1)
+def predict(model, segment_names, directory):
+    class_index = loader.build_class_index(directory)
+    batch = []
+    for segment_name in segment_names:
+        # load input data
+        fs, wave = utils.read_wave_file(segment_name)
+        Sxx = sp.wave_to_sample_spectrogram(wave, fs)
+        Sxx = scipy.misc.imresize(Sxx, (256, 512))
+        batch.append(Sxx)
+    batch = np.array(batch)
 
-    model = CubeRun(nb_classes=nb_classes, input_shape=input_shape)
+    batch = batch.reshape(batch.shape[0], batch.shape[1], batch.shape[2], 1)
 
-    model.load_weights("../weights/2016_11_16_06:31:03_cuberun.h5")
+    y_probs = model.predict(batch, batch_size=16, verbose=1)
+    y_cats = [int(np.argmax(y_prob)) for y_prob in y_probs]
+    species = [class_index[y_cat] for y_cat in y_cats]
 
-    return model
-
-def predict(model, filename):
-    fs, x = utils.read_wave_file("../datasets/mlsp2013/test/"+filename+".wav")
-
-    (f, t, Sxx) = utils.wave_to_spectrogram(x, fs)
-
-    X = np.array([Sxx])
-
-    X = X.reshape(X.shape[0], 257, 624, 1)
-
-    y = model.predict(X, batch_size=32, verbose=1)
-    y = np.round(y)
-
-    return [binary_to_id(v) for v in  y]
+    return species
 
 def binary_to_id(Y):
     i = 0
