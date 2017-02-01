3b72f4e922b59548db1cfc01f2d3cc9fff465f17
johnmartinsson <john.martinsson@gmail.com>
2017-02-01 13:54:14 +0100

    Change data splitter

--- split_data.py
+++ split_data.py
@@ -5,7 +5,7 @@ import numpy as np
 import shutil
 from bird import loader as l
 
-source_dir = "./datasets/birdClef2016Whole/"
+source_dir = "/disk/martinsson-spring17/birdClef2016Subset"
 classes = os.listdir(os.path.join(source_dir, "train"))
 
 percentage_validation_sampels = 0.10
@@ -24,15 +24,15 @@ for ((c, segments), p) in zip(unique_sampels, progress):
 
     valid_class_path = os.path.join(source_dir, "valid", c)
     if not os.path.exists(valid_class_path):
-        print("os.makedirs("+valid_class_path+")")
-        #os.makedirs(valid_class_path)
+        #print("os.makedirs("+valid_class_path+")")
+        os.makedirs(valid_class_path)
 
     i_valid_samples = np.random.choice(range(len(segments)),
                                        nb_validation_samples, replace=False)
     valid_samples = [segments[i] for i in i_valid_samples]
 
     for sample in valid_samples:
-        print(c, "validation")
+        #print(c, "validation")
         for segment in sample:
-            print("shutil.move("+segment+","+valid_class_path+")")
-            #shutil.move(segment, valid_class_path)
+            #print("shutil.move("+segment+","+valid_class_path+")")
+            shutil.move(segment, valid_class_path)
